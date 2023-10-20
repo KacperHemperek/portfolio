473a20222511b7475962cbe20b58689663259bf4
Kacper Hemperek <kacper.hemperek@o2.pl>
2023-10-20 10:58:24 +0200

    refactor: clean unused imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,3 @@
-import { GithubIcon, LinkedinIcon } from 'lucide-react';
-import Link from 'next/link';
-import Highlight from '~/components/common/hightlight';
-import IconLink from '~/components/common/icon-link';
-import OutsideLink from '~/components/common/outside-link';
 import HomeHeader from '~/components/homepage/home-header';
 
 export default function Home() {
