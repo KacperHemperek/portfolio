2d74a73cafde0e3ae1867690e14a4889fb1099ab
Kacper Hemperek <kacper.hemperek@o2.pl>
2023-11-07 17:31:45 +0100

    feat: add email in footer

--- src/components/homepage/footer.tsx
+++ src/components/homepage/footer.tsx
@@ -1,13 +1,21 @@
+import { Mail } from 'lucide-react';
 import React from 'react';
 import Highlight from '~/components/common/highlight';
 import { socials } from '~/utils/socials';
 
 export default function Footer() {
   return (
-    <footer className='py-24 text-center font-roboto text-sm text-white/50'>
+    <footer className='flex flex-col items-center justify-center gap-12 py-24 text-center font-roboto text-sm text-white/50'>
       <a href={socials.github.url} target='_blank'>
         Created by <Highlight>Kacper Hemperek</Highlight> <br />
         source code can be found here
+      </a>
+      <a
+        href={socials.email.url}
+        className='flex items-center gap-2 text-white/50'
+      >
+        <Mail className='h-4 w-4' />
+        <span>kacper.hemperek@o2.pl</span>
       </a>
     </footer>
   );
