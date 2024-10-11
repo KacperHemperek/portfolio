6adc8836f3b721e4504bddad6873e723be5f17fd
Kacper Hemperek <kacper.hemperek@apptimia.com>
2024-10-11 22:52:52 +0200

    refactor: move experience content out of the component

--- src/components/homepage/experience-section.tsx
+++ src/components/homepage/experience-section.tsx
@@ -1,53 +1,11 @@
 'use client';
 import React from 'react';
 import SectionHeading from '~/components/common/section-heading';
-import WorkItem, { WorkItemProps } from '~/components/common/work-item';
+import WorkItem from '~/components/common/work-item';
 import { motion } from 'framer-motion';
+import { workItemsList } from '~/content/experience';
 
 export default function ExperienceSection() {
-  const workItemsList: Omit<WorkItemProps, 'last'>[] = [
-    {
-      number: '02',
-      position: 'Frontend Developer',
-      companyName: 'Apptimia',
-      companyUrl: 'https://apptimia.com',
-      endDate: 'Present',
-      startDate: 'October 2022',
-      skills: [
-        'React',
-        'Electron',
-        'Angular 2+',
-        'TypeScript',
-        'styled-components',
-        'Angular Material',
-        'SASS',
-      ],
-      list: [
-        'Creating desktop application that is a part of a system used for testing RPAs with Electron, React and styled-components',
-        'Building block based UIs with react flow and using Electron APIs to read, save, translate files, capture desktop windows and much more',
-        'Maintaining angular application that is centered around working with a map and displaying data and photos on it. This application uses Mapbox GL JS, Angular Material and SASS',
-        'Writing unit test for React components as well as libraries that are used in the project and end to end tests for Angular application with Cypress',
-        'Working in a team of 3 developer with a project manager',
-        'Bringing designs from Figma and Adobe XD to life with React',
-      ],
-    },
-    {
-      number: '01',
-      position: 'Junior Frontend Developer',
-      companyName: 'Delivence',
-      companyUrl: 'https://www.linkedin.com/company/webbespokers/',
-      startDate: 'May 2022',
-      endDate: 'July 2022',
-      skills: ['React', 'TypeScript', 'TailwindCSS', 'Vue.js', 'Recharts'],
-      list: [
-        'Building data driven applications with React and TypeScript. Creating reusable components and using TailwindCSS for styling.',
-        'Creating graphs in React application with Recharts.',
-        'Learning Vue.js and creating a simple application with it.',
-        'Working in a team of a fullstack developer, frontend developer and project manager',
-      ],
-    },
-  ];
-
   return (
     <section id='experience'>
       <SectionHeading title='Experience' number='01' />
